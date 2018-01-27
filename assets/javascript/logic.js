8b95d33ab97db1e9e48f791ef33d5ae02a210024
paulaperoutka <paulaperoutka@gmail.com>
2018-01-27 08:37:34 -0500

    with notes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,4 @@
+//Firebase config
 const config = {
     apiKey: "AIzaSyDSBuaDZP7vTh--Jhztw6-rGWwIzX4ZXP4",
     authDomain: "train-timetable-78d37.firebaseapp.com",
@@ -7,12 +8,15 @@ const config = {
     messagingSenderId: "4449466252"
   };
 
+//Firebase initialize app
 firebase.initializeApp(config);
 
 const newTrain = {};
 
+//Add trains to firebase folder
 const dbRef = firebase.database().ref("timetable/trains");
 
+//Capture user input for new trains
 $("#add-train-btn").on("click", function (event) {
 	event.preventDefault();
 
@@ -28,6 +32,7 @@ $("#add-train-btn").on("click", function (event) {
   console.log(newTrain.first, "-new train first train");
   console.log(newTrain.frequency, "-new train frequency");
 
+//push user train to database
   dbRef.push(newTrain);
 
   resetInputs();
@@ -36,6 +41,7 @@ $("#add-train-btn").on("click", function (event) {
 
 });
 
+//Snapshot of dbRef on train addition
 dbRef.on("child_added", function(childSnapshot, prevChildKey) {
 
   // New Train Info
@@ -44,15 +50,18 @@ dbRef.on("child_added", function(childSnapshot, prevChildKey) {
   
   let time;
 
+//Calculate countdown in minutes to next train
   newTrain.minutesAway = moment(newTrain.first, "HH:mm").diff(moment(), "minutes");
 
   let countDown = newTrain.minutesAway;
   console.log(Math.sign(countDown));
 
+//If next train time has passed, recalculate minutes until next train
   if(Math.sign(countDown) === -1) {
     let newCountDown = countDown;
     console.log(newCountDown);
 
+//Add frequency of trains until closest to current hour
     while(Math.sign(newCountDown) === -1) {
       console.log(newTrain.frequency);
       newCountDown = newCountDown + parseInt(newTrain.frequency);
@@ -65,26 +74,29 @@ dbRef.on("child_added", function(childSnapshot, prevChildKey) {
     console.log(time);
   }
 
+//If next train is still coming, calculate the minutes away
   else {
     console.log(Math.sign(countDown));
     time = countDown;
     console.log(time);
   }
 
-  // Calculate the next arrival
+// Calculate the next train time for the schedule
   newTrain.nextArrival = moment().add(time, "minutes").format("HH:mm");
   let nextTrain = newTrain.nextArrival;
   console.log(nextTrain);
 
-  // Add each train to the table
+// Add each train & info to the table
   $("#train-table > tbody").append(createTrainRow(newTrain));
 
 }, 
 
+//Handling errors case
 function(errorObject) {
   console.log("Errors handled; " + errorObject.code);
 });
 
+//Create HTML row for each new train
 function createTrainRow(train) {
   const trainRow = $("<tr>");
   trainRow.append("<td>" + train.name + "</td>");
@@ -96,6 +108,7 @@ function createTrainRow(train) {
   return trainRow;
 }
 
+//Empty form upon train submission
 function resetInputs() {
 	$(".form-control").val("");
 }
